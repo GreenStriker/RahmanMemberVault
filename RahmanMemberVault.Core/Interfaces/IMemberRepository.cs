fab0358230f76ebed63d88d0e03b5c43dd034962
Md Mustafizur Rahman <moukhkst@gmail.com>
2025-04-18 21:28:21 -0700

    Added Repo Level interface for member

---
+++ RahmanMemberVault.Core/Interfaces/IMemberRepository.cs
@@ -0,0 +1,28 @@
+﻿using RahmanMemberVault.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RahmanMemberVault.Core.Interfaces
+{
+    public interface IMemberRepository
+    {
+        
+        /// Gets all members.
+        Task<IEnumerable<Member>> GetAllMembersAsync();
+        
+        /// Gets a member by ID.
+        Task<Member> GetMemberByIdAsync(int id);
+        
+        /// Adds a new member.
+        Task AddMemberAsync(Member member);
+        
+        /// Updates an existing member.
+        Task UpdateMemberAsync(Member member);
+
+        /// Deletes a member by ID.
+        Task DeleteMemberAsync(int id);
+    }
+}
