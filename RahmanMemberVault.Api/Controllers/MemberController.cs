00998cc2e5f2dfbe644f3a2e35c26ba8cb3d91a8
Md Mustafizur Rahman <moukhkst@gmail.com>
2025-05-03 16:04:58 -0700

    structure chnage

--- RahmanMemberVault.Api/Controllers/MemberController.cs
+++ RahmanMemberVault.Api/Controllers/MemberController.cs
@@ -24,6 +24,7 @@ namespace RahmanMemberVault.Api.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetAll()
         {
+            //throw new System.NullReferenceException("This is a test exception"); // Simulate an error for testing purposes
             var members = await _service.GetAllMembersAsync();
             return Ok(members); // Return 200 OK with the list of members
         }
